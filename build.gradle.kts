7ee23b73b1f397ff2459158d6c1aa820245d5311
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-10 19:08:20 +0200

    project structure
    migrate app & project gradle build files to kotlin dsl and add all needed dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Build.androidBuildTools)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath(Build.kotlinGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
