606e79ce21b16722e310bad1bead4322ed43befb
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-10 22:21:55 +0200

    Data layer setup
    implement FootballApi as ApiClient and add endpoint facade

---
+++ app/src/main/java/mahmoud/alim/premium/data/remote/api/FootballApi.kt
@@ -0,0 +1,37 @@
+package mahmoud.alim.premium.data.remote.api
+
+import mahmoud.alim.premium.BuildConfig
+import mahmoud.alim.premium.data.remote.dto.LeagueDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+import java.time.LocalDate
+
+/**
+ * @author Mahmoud Alim on 10/11/2022.
+ */
+interface FootballApi {
+
+    @GET("v4/competitions/PL/matches")
+    suspend fun getLeagueMatches(
+        @Header("X-Auth-Token") token: String,
+        @Query("limit") limit: Int? = null, //Integer [1-500]
+    ): Response<LeagueDto>
+
+
+    @GET("v4/competitions/{id}/matches")
+    suspend fun getLeagueMatchesWithinRange(
+        @Header("X-Auth-Token") token: String,
+        @Path("id") leagueId: String = "PL",
+        @Query("dateFrom") dateFrom: LocalDate,
+        @Query("dataTo") dataTo: LocalDate,
+        @Query("limit") limit: Int? = null,
+    ): Response<LeagueDto>
+
+
+    companion object {
+        const val BASE_URL = "https://api.football-data.org/"
+    }
+}
