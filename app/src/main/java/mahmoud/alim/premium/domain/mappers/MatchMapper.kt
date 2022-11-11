7297921b7adf28f298018adc9c92cb6e046e4375
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-11 20:45:56 +0200

    Data layer setup
    add MatchMapper to provide mappers to map to/from Fixture (domain-layer) dto  and (data-layer) dto

---
+++ app/src/main/java/mahmoud/alim/premium/domain/mappers/MatchMapper.kt
@@ -0,0 +1,55 @@
+package mahmoud.alim.premium.domain.mappers
+
+import mahmoud.alim.premium.data.local.dto.MatchEntity
+import mahmoud.alim.premium.data.remote.dto.Match
+import mahmoud.alim.premium.domain.model.Fixture
+import mahmoud.alim.premium.domain.model.MatchStatus
+
+/**
+ * @author Mahmoud Alim on 11/11/2022.
+ */
+
+
+fun Match.toFixture(): Fixture {
+    return Fixture(
+        id = id,
+        homeTeamName = homeTeam.shortName,
+        awayTeamName = awayTeam.shortName,
+        homeTeamFlag = homeTeam.crest,
+        awayTeamFlag = awayTeam.crest,
+        status = MatchStatus.fromString(status),
+        homeTeamScore = score?.fullTime?.home,
+        awayTeamScore = score?.fullTime?.away,
+        matchDate = utcDate
+    )
+}
+
+
+fun Fixture.toMatchEntity(): MatchEntity {
+    return MatchEntity(
+        id = id,
+        homeTeamName = homeTeamName,
+        awayTeamName = awayTeamName,
+        homeTeamFlag = homeTeamFlag,
+        awayTeamFlag = awayTeamFlag,
+        status = status.name,
+        homeTeamScore = homeTeamScore ?: 0,
+        awayTeamScore = awayTeamScore ?: 0,
+        matchDate = matchDate
+    )
+}
+
+
+fun MatchEntity.toFixture(): Fixture {
+    return Fixture(
+        id = id,
+        homeTeamName = homeTeamName,
+        awayTeamName = awayTeamName,
+        homeTeamFlag = homeTeamFlag,
+        awayTeamFlag = awayTeamFlag,
+        status = MatchStatus.fromString(status),
+        homeTeamScore = homeTeamScore ?: 0,
+        awayTeamScore = awayTeamScore ?: 0,
+        matchDate = matchDate
+    )
+}
