49357dea85ab1db30719a8eade2d9db7a2cd1a02
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-11 20:50:03 +0200

    Data layer setup
    provide all needed data Module dependencies

--- app/src/main/java/mahmoud/alim/premium/data/di/DataModule.kt
+++ app/src/main/java/mahmoud/alim/premium/data/di/DataModule.kt
@@ -1,10 +1,19 @@
 package mahmoud.alim.premium.data.di
 
+import android.app.Application
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import mahmoud.alim.premium.data.local.datasource.LocalDataSrc
+import mahmoud.alim.premium.data.local.datasource.LocalDataSrcImpl
+import mahmoud.alim.premium.data.local.db.MatchesDatabase
 import mahmoud.alim.premium.data.remote.api.FootballApi
+import mahmoud.alim.premium.data.remote.datasource.RemoteDataSrc
+import mahmoud.alim.premium.data.remote.datasource.RemoteDataSrcImpl
+import mahmoud.alim.premium.data.repo.FootballFixtureRepo
+import mahmoud.alim.premium.data.repo.FootballFixtureRepoImpl
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -42,4 +51,37 @@ object DataModule {
             .build()
             .create()
     }
+
+    @Provides
+    @Singleton
+    fun provideMatchesDatabase(app: Application): MatchesDatabase {
+        return Room.databaseBuilder(
+            app,
+            MatchesDatabase::class.java,
+            MatchesDatabase.DB_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteDataSource(api: FootballApi): RemoteDataSrc {
+        return RemoteDataSrcImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocalDataSource(database: MatchesDatabase): LocalDataSrc {
+        return LocalDataSrcImpl(database.dao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFootballFixtureRepo(
+        remoteDataSrc: RemoteDataSrc,
+        localDataSrc: LocalDataSrc
+    ): FootballFixtureRepo {
+        return FootballFixtureRepoImpl(remote = remoteDataSrc, local = localDataSrc)
+    }
+
+
 }
