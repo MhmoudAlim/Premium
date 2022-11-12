eedae7202ef072a1f338e5f3e29c0015b60bf930
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-12 21:56:53 +0200

    UI layer setup
    add Route object to define all app screens routes

---
+++ app/src/main/java/mahmoud/alim/premium/core/navigation/Route.kt
@@ -0,0 +1,9 @@
+package mahmoud.alim.premium.core.navigation
+
+/**
+ * @author Mahmoud Alim on 12/11/2022.
+ */
+object Route {
+    const val Fixture = "fixture"
+    const val Favourites = "favourites"
+}
