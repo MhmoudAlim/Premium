64f13faaca3145e20aac3e060d23dcbd96f23829
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-12 22:40:04 +0200

    UI layer setup
    implement HomeScaffold UI component

---
+++ app/src/main/java/mahmoud/alim/premium/ui/components/Scaffold.kt
@@ -0,0 +1,65 @@
+package mahmoud.alim.premium.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import mahmoud.alim.premium.R
+
+/**
+ * @author Mahmoud Alim on 12/11/2022.
+ */
+
+@Composable
+fun HomeScaffold(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    Column(
+        modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(bottomEnd = 24.dp, bottomStart = 24.dp))
+                .background(MaterialTheme.colors.primary)
+                .padding(top = 16.dp)
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment= Alignment.Bottom
+        ) {
+            Text(
+                text = stringResource(R.string.premium_league),
+                style = TextStyle.Default.copy(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colors.onPrimary
+                ),
+                maxLines = 1,
+            )
+        }
+        Row(
+            modifier = modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            content()
+        }
+    }
+}
