b656d5b0c98c3e0bcac48d2204c739a3241fdef6
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-12 22:41:38 +0200

    UI layer setup
    add NavHost and all app screens to nav graph to MainActivity

--- app/src/main/java/mahmoud/alim/premium/ui/MainActivity.kt
+++ app/src/main/java/mahmoud/alim/premium/ui/MainActivity.kt
@@ -3,24 +3,40 @@ package mahmoud.alim.premium.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
+import androidx.compose.material.rememberScaffoldState
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import mahmoud.alim.premium.core.navigation.Route
+import mahmoud.alim.premium.ui.components.HomeScaffold
 import mahmoud.alim.premium.ui.theme.PremiumTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PremiumTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
+                val navController = rememberNavController()
+                val scaffoldState = rememberScaffoldState()
+                HomeScaffold {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.Fixture
+                    ) {
+                        composable(Route.Fixture) {
+
+                        }
+                        composable(Route.Favourites) {
+
+                        }
+
+                    }
+
                 }
             }
         }
+
     }
 }
