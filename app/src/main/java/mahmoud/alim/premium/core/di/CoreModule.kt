9d9b859608381951ecad7e9220f4c9639aacb902
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-12 21:39:37 +0200

    UI layer setup
    implement CoreModule and add provideDispatchers to inject CoroutineDispatcher in viewmodel

---
+++ app/src/main/java/mahmoud/alim/premium/core/di/CoreModule.kt
@@ -0,0 +1,31 @@
+package mahmoud.alim.premium.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import mahmoud.alim.premium.core.dispactchers.DispatcherProvider
+import javax.inject.Singleton
+
+/**
+ * @author Mahmoud Alim on 12/11/2022.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object CoreModule {
+
+    @Singleton
+    @Provides
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
+}
