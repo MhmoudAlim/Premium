bc0b33835cfed95b2c62ff8c76558f0c8f3ab8ed
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-12 22:47:45 +0200

    UI layer setup
    add Dimensions to define all app to be used padding Dimensions

---
+++ app/src/main/java/mahmoud/alim/premium/ui/util/Dimensions.kt
@@ -0,0 +1,19 @@
+package mahmoud.alim.premium.ui.util
+
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * @author Mahmoud Alim on 12/11/2022.
+ */
+data class Dimensions(
+    val default: Dp = 0.dp,
+    val spaceExtraSmall: Dp = 4.dp,
+    val spaceSmall: Dp = 8.dp,
+    val spaceMedium: Dp = 16.dp,
+    val spaceLarge: Dp = 32.dp,
+    val spaceExtraLarge: Dp = 64.dp,
+)
+
+val LocalSpacing = compositionLocalOf { Dimensions() }
