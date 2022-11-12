c4ef80ecd699ce6b38a157612781ba0bce7f6a1c
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-12 21:22:56 +0200

    UI layer setup
    implement FixtureState state class to save Fixture screen state

---
+++ app/src/main/java/mahmoud/alim/premium/ui/screen/fixture/state/FixtureState.kt
@@ -0,0 +1,12 @@
+package mahmoud.alim.premium.ui.screen.fixture.state
+
+import mahmoud.alim.premium.domain.model.Fixture
+
+/**
+ * @author Mahmoud Alim on 12/11/2022.
+ */
+
+data class FixtureState(
+    val fixtures: List<Fixture> = emptyList(),
+    val isSearching: Boolean = true
+)
