73212101cf3dbbbe3adfdf9b77a900d984145fe8
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-13 00:23:39 +0200

    implement FilterAndGroupMatches to add grouping by day logic to fixtures list

--- app/src/main/java/mahmoud/alim/premium/domain/usecase/FilterAndGroupMatches.kt
+++ app/src/main/java/mahmoud/alim/premium/domain/usecase/FilterAndGroupMatches.kt
@@ -1,12 +1,18 @@
 package mahmoud.alim.premium.domain.usecase
 
+import mahmoud.alim.premium.domain.model.Fixture
+
 
 /**
  * @author Mahmoud Alim on 12/11/2022.
  */
 class FilterAndGroupMatches {
 
-    operator fun invoke() {
-
+    operator fun invoke(fixtures: List<Fixture>): List<Pair<String, List<Fixture>>> {
+        return fixtures.groupBy {
+            it.matchDate
+        }.entries.map { (date, fixtures) ->
+            Pair(date, fixtures)
+        }
     }
 }
