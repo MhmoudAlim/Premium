2f9ff6e12b292341ef4711c07004edd5f30ab825
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-12 21:20:52 +0200

    UI layer setup
    add application class to instantiate hilt

---
+++ app/src/main/java/mahmoud/alim/premium/core/app/PremiumApp.kt
@@ -0,0 +1,12 @@
+package mahmoud.alim.premium.core.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author Mahmoud Alim on 12/11/2022.
+ */
+
+@HiltAndroidApp
+class PremiumApp : Application() {
+}
