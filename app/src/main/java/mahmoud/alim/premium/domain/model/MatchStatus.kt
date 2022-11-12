91183b7a85f8ef8e814d4d60c6e1ec8cfdcbbfec
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-12 21:20:12 +0200

    UI layer setup
    fix MatchStatus Ended state name

--- app/src/main/java/mahmoud/alim/premium/domain/model/MatchStatus.kt
+++ app/src/main/java/mahmoud/alim/premium/domain/model/MatchStatus.kt
@@ -5,19 +5,19 @@ package mahmoud.alim.premium.domain.model
  */
 
 sealed class MatchStatus(val name: String) {
-    object Ended : MatchStatus(ENDED)
+    object Ended : MatchStatus(FINISHED)
     object Scheduled : MatchStatus(SCHEDULED)
 
     companion object {
         fun fromString(name: String): MatchStatus {
             return when (name) {
-                ENDED -> Ended
+                FINISHED -> Ended
                 SCHEDULED -> Scheduled
                 else -> Scheduled
             }
         }
 
-        private const val ENDED = "ENDED"
+        private const val FINISHED = "FINISHED"
         private const val SCHEDULED = "SCHEDULED"
     }
 }
