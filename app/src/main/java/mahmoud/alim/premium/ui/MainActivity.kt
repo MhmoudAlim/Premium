52789433d3c0a606dd0cd4d112d92ed93a0a3e7f
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-13 23:21:23 +0200

    add fav screen to MainActivity

--- app/src/main/java/mahmoud/alim/premium/ui/MainActivity.kt
+++ app/src/main/java/mahmoud/alim/premium/ui/MainActivity.kt
@@ -3,14 +3,13 @@ package mahmoud.alim.premium.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.rememberScaffoldState
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import mahmoud.alim.premium.core.navigation.Route
 import mahmoud.alim.premium.core.navigation.navigate
-import mahmoud.alim.premium.ui.components.HomeScaffold
+import mahmoud.alim.premium.ui.screen.favourites.FavouritesScreen
 import mahmoud.alim.premium.ui.screen.fixture.FixturesScreen
 import mahmoud.alim.premium.ui.theme.PremiumTheme
 
@@ -28,9 +27,8 @@ class MainActivity : ComponentActivity() {
                     composable(Route.Fixture) {
                         FixturesScreen(onNavigate = navController::navigate)
                     }
-
                     composable(Route.Favourites) {
-
+                        FavouritesScreen()
                     }
 
                 }
