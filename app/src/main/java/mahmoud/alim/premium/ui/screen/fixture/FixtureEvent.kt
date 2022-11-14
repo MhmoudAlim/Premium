cfdcb7c0ff83fb41b6a9530e5cfcb744452761a4
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-14 20:35:50 +0200

    add new ShowAllFixtures() UI event to FixtureEvent() to decide viewTypes of Ui state

--- app/src/main/java/mahmoud/alim/premium/ui/screen/fixture/FixtureEvent.kt
+++ app/src/main/java/mahmoud/alim/premium/ui/screen/fixture/FixtureEvent.kt
@@ -6,7 +6,8 @@ import mahmoud.alim.premium.domain.model.Fixture
  * @author Mahmoud Alim on 12/11/2022.
  */
 sealed class FixtureEvent {
-    object OnLoadAllFixtures : FixtureEvent()
+    object OnLoadUpcomingFixtures : FixtureEvent()
     object OnNavigateToFavourites : FixtureEvent()
+    data class ShowAllFixtures(val showAll: Boolean) : FixtureEvent()
     data class OnAddFixtureToFavClicked(val fixture: Fixture) : FixtureEvent()
 }
