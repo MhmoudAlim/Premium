0f31b4835de9bd420216a0c5820fcee87d310a46
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-14 20:27:32 +0200

    Add new FilterUpcomingMatches useCase

---
+++ app/src/main/java/mahmoud/alim/premium/domain/usecase/FilterUpcomingMatches.kt
@@ -0,0 +1,16 @@
+package mahmoud.alim.premium.domain.usecase
+
+import mahmoud.alim.premium.core.AppDateTime
+import mahmoud.alim.premium.domain.model.Fixture
+
+/**
+ * @author Mahmoud Alim on 14/11/2022.
+ */
+class FilterUpcomingMatches {
+
+    operator fun invoke(
+        fixtures: List<Pair<String, List<Fixture>>>,
+    ) = fixtures.filter { (date, _) ->
+        AppDateTime.durationInDaysOf(date)
+    }
+}
