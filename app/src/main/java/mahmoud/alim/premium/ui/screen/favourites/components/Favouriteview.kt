c72404e4dafac9f46ff11adb7501a9582b6420a3
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-14 16:09:04 +0200

    update favourites view ui padding

--- app/src/main/java/mahmoud/alim/premium/ui/screen/favourites/components/Favouriteview.kt
+++ app/src/main/java/mahmoud/alim/premium/ui/screen/favourites/components/Favouriteview.kt
@@ -1,12 +1,16 @@
 package mahmoud.alim.premium.ui.screen.favourites.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import mahmoud.alim.premium.domain.model.Fixture
 import mahmoud.alim.premium.ui.components.MatchMetaDetailsView
 import mahmoud.alim.premium.ui.components.TeamFlagView
@@ -25,17 +29,20 @@ fun FavouriteView(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(
-                start = spacing.spaceSmall,
-                top = spacing.spaceSmall,
-                bottom = spacing.spaceSmall
-            ),
+            .clip(shape = RoundedCornerShape(spacing.spaceMedium))
+            .background(
+                color = MaterialTheme.colors.primary.copy(.2f),
+                shape = RoundedCornerShape(spacing.spaceMedium)
+            )
+            .padding(spacing.spaceSmall),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Row(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = spacing.spaceSmall)
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
