351ebbe2ba118a40cc24360176b9dd73e1dae218
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-14 16:14:51 +0200

    add logic to show/hide progressbar based on state.isSearching, add animated visibilty to fixtures list

--- app/src/main/java/mahmoud/alim/premium/ui/screen/fixture/FixturesScreen.kt
+++ app/src/main/java/mahmoud/alim/premium/ui/screen/fixture/FixturesScreen.kt
@@ -1,14 +1,19 @@
 package mahmoud.alim.premium.ui.screen.fixture
 
 import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -47,23 +52,31 @@ fun FixturesScreen(
     HomeScaffold(onNavigate = {
         viewModel.onEvent(FixtureEvent.OnNavigateToFavourites)
     }) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = spacing.spaceMedium),
+        AnimatedVisibility(visible = state.isSearching) {
+            CircularProgressIndicator()
+        }
+        AnimatedVisibility(
+            visible = !state.isSearching,
+            enter = slideInVertically()
+                    + expandVertically(expandFrom = Alignment.Top)
         ) {
-            items(state.fixtures) { (date, fixtures) ->
-                FixturesGroupView(
-                    date = date,
-                    fixtures = fixtures
-                ) {
-                    viewModel.onEvent(
-                        FixtureEvent.OnAddFixtureToFavClicked(it)
-                    )
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = spacing.spaceMedium),
+            ) {
+                items(state.fixtures) { (date, fixtures) ->
+                    FixturesGroupView(
+                        date = date,
+                        fixtures = fixtures
+                    ) {
+                        viewModel.onEvent(
+                            FixtureEvent.OnAddFixtureToFavClicked(it)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(spacing.spaceMedium))
                 }
-                Spacer(modifier = Modifier.height(spacing.spaceMedium))
             }
-
         }
     }
 }
