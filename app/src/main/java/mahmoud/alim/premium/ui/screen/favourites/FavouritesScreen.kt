72d439b9a62b95a1f617e79c685a0b47474aac97
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-14 16:09:48 +0200

    update favourites Screen ui components, add top bar

--- app/src/main/java/mahmoud/alim/premium/ui/screen/favourites/FavouritesScreen.kt
+++ app/src/main/java/mahmoud/alim/premium/ui/screen/favourites/FavouritesScreen.kt
@@ -1,13 +1,31 @@
 package mahmoud.alim.premium.ui.screen.favourites
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import mahmoud.alim.premium.R
 import mahmoud.alim.premium.ui.screen.favourites.components.FavouriteView
 import mahmoud.alim.premium.ui.util.LocalSpacing
 
@@ -23,15 +41,44 @@ fun FavouritesScreen(
     LaunchedEffect(Unit) {
         viewModel.onEvent(FavouritesEvent.OnLoadAllFixtures)
     }
-
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = spacing.spaceMedium),
-    )
-    {
-        items(state.favourites) {
-            FavouriteView(fixture = it)
+    Column(
+        Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(
+                    RoundedCornerShape(
+                        bottomEnd = spacing.spaceMedium,
+                        bottomStart = spacing.spaceMedium
+                    )
+                )
+                .background(MaterialTheme.colors.primary)
+                .padding(spacing.spaceMedium),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.favourites),
+                style = TextStyle(
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colors.onPrimary
+                )
+            )
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(spacing.spaceSmall),
+        ) {
+            items(state.favourites) {
+                FavouriteView(fixture = it)
+                Spacer(modifier = Modifier.height(spacing.spaceSmall))
+            }
         }
     }
 }
