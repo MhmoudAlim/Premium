61e1a2e04e1a8896e1f4e60615c1aecfa70a18a3
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-15 14:09:39 +0200

    refactor FixtureView ui components alignment, add onFavFixtureClick icon interactions

--- app/src/main/java/mahmoud/alim/premium/ui/screen/fixture/components/FixtureView.kt
+++ app/src/main/java/mahmoud/alim/premium/ui/screen/fixture/components/FixtureView.kt
@@ -3,14 +3,21 @@ package mahmoud.alim.premium.ui.screen.fixture.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,7 +34,6 @@ import mahmoud.alim.premium.ui.util.LocalSpacing
  * @author Mahmoud Alim on 12/11/2022.
  */
 
-
 @Composable
 fun FixtureView(
     fixture: Fixture,
@@ -35,6 +41,9 @@ fun FixtureView(
     onFavFixtureClick: () -> Unit
 ) {
     val spacing = LocalSpacing.current
+    var favIconTint by remember { mutableStateOf(Color.DarkGray) }
+    var favIcon by remember { mutableStateOf(Icons.Outlined.Add) }
+
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -52,34 +61,44 @@ fun FixtureView(
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(spacing.spaceExtraSmall)
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier.weight(.4f)
             ) {
-                TeamFlagView(fixture.homeTeamFlag, fixture.homeTeamName)
                 TeamNameView(fixture.homeTeamName)
+                Spacer(modifier = Modifier.width(spacing.spaceExtraSmall))
+                TeamFlagView(fixture.homeTeamFlag, fixture.homeTeamName)
             }
-
-            MatchMetaDetailsView(fixture)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.weight(.2f)
+            ) {
+                MatchMetaDetailsView(fixture)
+            }
 
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(spacing.spaceExtraSmall)
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier.weight(.4f)
             ) {
-                TeamNameView(fixture.awayTeamName)
                 TeamFlagView(fixture.awayTeamFlag, fixture.awayTeamName)
+                Spacer(modifier = Modifier.width(spacing.spaceExtraSmall))
+                TeamNameView(fixture.awayTeamName)
             }
         }
-
         Row(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.weight(.1f)
         ) {
             IconButton(onClick = {
+                favIconTint = Color.Yellow
+                favIcon = Icons.Outlined.Star
                 onFavFixtureClick()
             }) {
                 Icon(
-                    imageVector = Icons.Outlined.Star,
-                    tint = Color.Yellow,
+                    imageVector = favIcon,
+                    tint = favIconTint,
                     contentDescription = stringResource(id = R.string.favourite_fixture),
                     modifier = Modifier.size(20.dp)
                 )
@@ -87,7 +106,3 @@ fun FixtureView(
         }
     }
 }
-
-
-
-
