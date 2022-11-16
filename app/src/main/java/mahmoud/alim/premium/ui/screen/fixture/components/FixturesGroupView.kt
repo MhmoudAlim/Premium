c4b46571d1b8a98de2597f2e54b5ad19eac811e7
Mahmoud H. Alim <mahmoudhusseinalim@gmail.com>
2022-11-17 01:12:41 +0200

    update FixturesGroupView ui

--- app/src/main/java/mahmoud/alim/premium/ui/screen/fixture/components/FixturesGroupView.kt
+++ app/src/main/java/mahmoud/alim/premium/ui/screen/fixture/components/FixturesGroupView.kt
@@ -1,16 +1,14 @@
 package mahmoud.alim.premium.ui.screen.fixture.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
@@ -18,6 +16,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
 import mahmoud.alim.premium.domain.model.Fixture
+import mahmoud.alim.premium.ui.components.FixtureView
 import mahmoud.alim.premium.ui.util.LocalSpacing
 
 /**
@@ -34,11 +33,6 @@ fun FixturesGroupView(
     val spacing = LocalSpacing.current
     Column(
         modifier = modifier
-            .clip(shape = RoundedCornerShape(spacing.spaceMedium))
-            .background(
-                color = Color.LightGray.copy(.8f),
-                shape = RoundedCornerShape(spacing.spaceMedium)
-            )
             .semantics {
                 contentDescription = "Fixture"
             },
@@ -50,28 +44,15 @@ fun FixturesGroupView(
                 .padding(spacing.spaceSmall)
                 .padding(start = spacing.spaceSmall),
             style = TextStyle(
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                color = MaterialTheme.colors.primary
             ),
             overflow = TextOverflow.Ellipsis,
             maxLines = 1
         )
         Column(
-            modifier = modifier
-                .clip(
-                    shape = RoundedCornerShape(
-                        bottomEnd = spacing.spaceMedium,
-                        bottomStart = spacing.spaceMedium
-                    )
-                )
-                .background(
-                    color = MaterialTheme.colors.primary.copy(.2f),
-                    shape = RoundedCornerShape(
-                        bottomEnd = spacing.spaceMedium,
-                        bottomStart = spacing.spaceMedium
-                    )
-                )
-                .padding(spacing.spaceMedium),
+            modifier = modifier,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             fixtures.forEach {
@@ -79,6 +60,7 @@ fun FixturesGroupView(
                     onFavFixtureClick(it)
                 }
             }
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
 }
